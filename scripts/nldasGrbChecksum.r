8b8c86d4f42254a924aaa596fdd338237800ad45
Neil Best <nbest@ci.uchicago.edu>
2013-09-10 16:35:16 -0500

    download GRBs and verify checksums using GNU parallel. working on rendering Makeflow using whisker (mustache for R)

--- scripts/nldasGrbChecksum.r
+++ scripts/nldasGrbChecksum.r
@@ -2,40 +2,43 @@
 
 ## --interactive
 
-xmlFn <- argv[ 1]
+grbFn <- argv[ 1]
+xmlFn <- argv[ 2]
 
-grbFn <- gsub( ".xml$", "", xmlFn)
 
 library( XML, quietly= TRUE)
+library( stringr)
+
 
 ## xmlFn <-
 ##  "data/NLDAS_FORA0125_H.002/1979/001/NLDAS_FORA0125_H.A19790101.1300.002.grb.xml"
+## grbFn <- gsub( ".xml$", "", xmlFn)
 
-nldasMeta <-
-  xmlParseDoc( xmlFn)
-
-checksumExpected <-
-  as.numeric(
-    xpathSApply(
-      nldasMeta,
-      "//CheckSumValue",
-      xmlValue))
-
-library( stringr)
-
-checksumComputed <-
-  as.numeric(
-    str_split_fixed(
-      system(
-        ## paste( "cksum data/NLDAS_FORA0125_H.002/1979/001",
-        ##   "NLDAS_FORA0125_H.A19790101.1300.002.grb",
-        ##   sep= "/"),
-        paste( "cksum", grbFn),
-        intern= TRUE),
-      pattern= " ",
-      n= 3)[, 1])
-
-if( checksumComputed != checksumExpected) {
-  cat( grbFn, ": checksum mismatch . . . deleting.")
-  ## file.remove( xmlFn, grbFn)
+isValidChecksum <- function(
+  grbFn,
+  xmlFn= paste( grbFn, sep=".", "xml")
+  {
+    nldasMeta <-
+      xmlParseDoc( xmlFn)
+    checksumExpected <-
+      as.numeric(
+        xpathSApply(
+          nldasMeta,
+          "//CheckSumValue",
+          xmlValue))
+    checksumComputed <-
+      as.numeric(
+        str_split_fixed(
+          system(
+            paste( "cksum", grbFn),
+            intern= TRUE),
+          pattern= " ",
+          n= 3)[, 1])
+    checksumComputed != checksumExpected
+  }
+  
+if( !isValidChecksum( grbFn)) {
+  cat( grbFn, ": checksum mismatch\n")
+  file.remove( xmlFn, grbFn)
+  quit( save= "no", status= 10, runLast= FALSE)
 }
